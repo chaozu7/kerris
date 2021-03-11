c4afd571efd80fdbf216d786937611568af2edb6
chaozu7 <kamabo.7@gmail.com>
2021-03-11 09:58:27 +0100

    files

---
+++ index.php
@@ -0,0 +1,64 @@
+<body>
+    <header>
+        <section class="hader-left">
+            <div class="logo">
+                <div class="logo__box">
+                    <img src="assets/logo.svg" alt="logo">
+                </div>
+            </div>
+            <div class="title">
+                <h1>
+                    Lorem ipsum dolor sit amet
+                </h1>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+                    nostrud exercitation ullamco.
+                </p>
+                <button>
+                    Learn more
+                </button>
+
+            </div>
+            <div class="headlines">
+                <h2> How it works?</h2>
+                <img src="assets/yellow_dot.svg">
+            </div>
+        </section>
+        <section class="header-right">
+            <div class="bcg">
+                <div class="bcg__lines">
+                    <img src="assets/lines.svg">
+                </div>
+                <div class="bcg__circles">
+                    <img src="assets/ovals.svg">
+                </div>
+            </div>
+        </section>
+    </header>
+    <main>
+        <section class="features">
+            <div class="features__block">
+                <div class="features__block-head">
+                    <img src="assets/block.svg">
+                    <img src="assets/head_1.svg">
+                    <h3>Block the cheaters</h3>
+                </div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
+                    et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex ea commodo consequat.</p>
+            </div>
+            <div class="features__block">
+                <div class="features__block-head">
+                    <img src="assets/find.svg">
+                    <img src="assets/head_2.svg">
+                    <h3>Block the cheaters</h3>
+                </div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
+                    et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex ea commodo consequat.</p>
+            </div>
+            </div>
+        </section>
+    </main>
+</body>
